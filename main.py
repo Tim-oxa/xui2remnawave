aba3b5ee2e9cd4e251218008529a593fb133faaf
Tim <113267366+Tim-oxa@users.noreply.github.com>
2025-06-01 23:45:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import json
 import re
 
 xui_url = input("3X-UI panel url: ")
+if xui_url.endswith("/"):
+    xui_url = xui_url[:-1]
 xui_username = input("3X-UI username: ")
 xui_password = input("3X-UI password: ")
 
 remna_url = input("Remnawave panel url: ")
+if remna_url.endswith("/"):
+    remna_url = remna_url[:-1]
 remna_token = input("Remnawave API token: ")
 
 inbound_id = input("3X-UI inbound id: ")
