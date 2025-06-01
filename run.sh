d2d13ac4d7db9e4ce38350ae599189af6e105fe7
Tim <113267366+Tim-oxa@users.noreply.github.com>
2025-06-01 22:34:38 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-set -e
-
-VENV_DIR="$HOME/xui2remnawave-env"
-SCRIPT_PATH="$HOME/xui2remnawave.py"
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-echo -e "${GREEN}[1/6] Обновление пакетов...${NC}"
-sudo apt update
-
-echo -e "${GREEN}[2/6] Установка Python, pip и необходимых пакетов...${NC}"
-sudo apt install -y python3-full python3-venv python3-pip curl
-
-echo -e "${GREEN}[3/6] Создание виртуального окружения...${NC}"
+VENV_DIR=$(mktemp -d)
 python3 -m venv "$VENV_DIR"
-
-echo -e "${GREEN}[4/6] Активация окружения и установка httpx...${NC}"
 source "$VENV_DIR/bin/activate"
-"$VENV_DIR/bin/pip" install --upgrade pip
-"$VENV_DIR/bin/pip" install httpx
 
-echo -e "${GREEN}[5/6] Скачивание скрипта...${NC}"
-curl -sSL https://raw.githubusercontent.com/Tim-oxa/xui2remnawave/main/main.py -o "$SCRIPT_PATH"
+pip install httpx > /dev/null
 
-echo -e "${GREEN}[6/6] Запуск скрипта...${NC}"
-"$VENV_DIR/bin/python" "$SCRIPT_PATH"
+curl -s "https://github.com/Tim-oxa/xui2remnawave/raw/refs/heads/main/main.py" | python3
 
-echo -e "${GREEN}✅ Готово! Скрипт завершил работу.${NC}"
+deactivate
+rm -rf "$VENV_DIR"
